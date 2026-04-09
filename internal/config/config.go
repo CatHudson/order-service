3821fa5841352833694fe3b6c4dddc56c110c3e6
CatHudson <72917346+CatHudson@users.noreply.github.com>
2026-04-09 12:20:33 +0400

    Day 9 - config and Postgres impl

---
+++ internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	GRPC     GRPCConfig     `yaml:"grpc"`
+	Postgres PostgresConfig `yaml:"postgres"`
+}
+
+type GRPCConfig struct {
+	Network string `yaml:"network"`
+	Port    int    `yaml:"port"`
+}
+
+type PostgresConfig struct {
+	Driver          string        `yaml:"driver"`
+	Host            string        `yaml:"host"`
+	Port            int           `yaml:"port"`
+	User            string        `yaml:"user"`
+	Password        string        `yaml:"password"`
+	Database        string        `yaml:"database"`
+	SSLMode         string        `yaml:"ssl_mode"`
+	MaxOpenConns    int           `yaml:"max_open_conns"`
+	MaxIdleConns    int           `yaml:"max_idle_conns"`
+	ConnMaxLifetime time.Duration `yaml:"conn_max_lifetime"`
+	ConnMaxIdleTime time.Duration `yaml:"conn_max_idle_time"`
+}
+
+func (p PostgresConfig) DSN() string {
+	hostPort := net.JoinHostPort(p.Host, fmt.Sprintf("%d", p.Port))
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s/%s?sslmode=%s",
+		p.User, p.Password, hostPort, p.Database, p.SSLMode,
+	)
+}
+
+func Load(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not load config: %w", err)
+	}
+
+	var config Config
+	if err = yaml.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("could not parse config: %w", err)
+	}
+
+	return &config, nil
+}
